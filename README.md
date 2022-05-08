ddbb348c3793bf853cb9dcf6f9e708686e3633a2
Laurent Demailly <ldemailly@gmail.com>
2022-05-08 10:39:33 -0700

    buildinfo can get us the version

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Fortio simple TLS/ingress proxy
 
 Front end for running fortio report for instance standalone with TLS / Autocert
+
+# Install
+
+using golang 1.18+
+
+```
+go install fortio.org/proxy@latest
+```
--- proxy_main.go
+++ proxy_main.go
@@ -10,19 +10,21 @@ import (
 	"fortio.org/proxy/config"
 )
 
-var (
-	version = "dev"
-)
-
 func main() {
 	configs := dflag.DynJSON(flag.CommandLine, "routes.json", &[]config.Route{}, "json list of `routes`")
+	fullVersion := flag.Bool("version", false, "Show full version info")
 	configDir := flag.String("config", "",
 		"Config directory `path` to watch for changes of dynamic flags (empty for no watch)")
-
-	log.Infof("Fortio Proxy %s starting", version)
-	binfo, _ := debug.ReadBuildInfo()
-	log.Infof("Buildinfo: %s", binfo.String())
 	flag.Parse()
+	version := "unknown"
+	binfo, ok := debug.ReadBuildInfo()
+	if ok {
+		version = binfo.Main.Version
+	}
+	log.Infof("Fortio Proxy %s starting", version)
+	if *fullVersion {
+		log.Infof("Buildinfo: %s", binfo.String())
+	}
 	if *configDir != "" {
 		if _, err := configmap.Setup(flag.CommandLine, *configDir); err != nil {
 			log.Critf("Unable to watch config/flag changes in %v: %v", *configDir, err)
